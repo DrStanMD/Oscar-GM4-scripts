ba3439f0ae3b023a1d05ac12d1c0113d594c1a56
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2018-03-18 15:07:55 -0700

    Update Lab_cumulative_result_grid_.user.js

--- Lab_cumulative_result_grid_.user.js
+++ Lab_cumulative_result_grid_.user.js
@@ -4,7 +4,7 @@
 // @include     *lab/CA/ALL/labDisplay.jsp?segmentID*
 // @include     *lab/CA/ALL/labDisplay.jsp?demographicId*
 // @require   http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js
-// @version     1.1
+// @version     2.0
 // @grant       none
 // ==/UserScript==
 //========Get Path============
