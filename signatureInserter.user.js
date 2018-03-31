bff45e13ae60611b320d9dc6fd3d971e530c5216
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2018-03-31 10:24:51 -0700

    Update signatureInserter.user.js

--- signatureInserter.user.js
+++ signatureInserter.user.js
@@ -11,39 +11,27 @@ var elements = (window.location.pathname.split('/', 2))
 firstElement = (elements.slice(1))
 vPath = ('https://' + location.host + '/' + firstElement)
 
-$(function(){
-     $('iframe[name="lightwindow_iframe"]').load(function(){
-       alert('HI')   
-       //$(this).contents().find('body').append('<div>.....</div>')
-      });
-});
-
+setTimeout(function(){ 
 if(document.getElementById('canvas').getContext('2d')){
- // alert()
 var ctx = document.getElementById('canvas').getContext('2d')
- 
 base_image = new Image();
 base_image.src = vPath + '/eform/displayImage.do?imagefile=My Signature.jpg'
-
-//window.addEventListener('load', function () {
-setTimeout(function(){ 
   //alert("Hello")
   ctx.drawImage(base_image, 0, 0, 500, 100);
   ctx.beginPath();
   ctx.lineWidth = '2';
-  // ctx.strokeStyle = 'green'; // Green path
+  ctx.strokeStyle = 'green'; // Green path
   ctx.moveTo(0, 75);
   //Math.floor(Math.random() * (max - min + 1)) + min;
   var rnd = Math.floor(Math.random() * (100 - 40 + 1)) + 40;
   ctx.lineTo(250, rnd);
   ctx.stroke(); // Draw it
   ctx.beginPath();
-  // ctx.strokeStyle = 'purple'; // Purple path
+  ctx.strokeStyle = 'purple'; // Purple path
   var rnd1 = Math.floor(Math.random() * (200 - 0 + 1)) + 0;
   ctx.moveTo(rnd1, 0);
   ctx.lineTo(150, 130);
-
   ctx.stroke(); // Draw it
-//}, false);
-  }, 1000);
 }
+  }, 2000);
+
