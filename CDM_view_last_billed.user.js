88938e302dc3a9f3376a93b0da2865ad76cb4284
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2018-04-07 08:24:43 -0700

    Update CDM_view_last_billed.user.js

--- CDM_view_last_billed.user.js
+++ CDM_view_last_billed.user.js
@@ -7,10 +7,18 @@
 // @grant       none
 // ==/UserScript==
 //******************************************
+function fillfield(data1, data2) {
+  $('#keyword').css('background-color', data1);
+  $('#keyword').val(data2)
+  $('#keyword').click(function(){
+  $('#keyword').css('background-color', "white")
+  $('#keyword').val("")
+}); 
+}
 var elements = (window.location.pathname.split('/', 2))
 firstElement = (elements.slice(1)) //alert(firstElement)
-vPath = ('https://' + location.host + '/' + firstElement + '/') 
-var myParam = location.search.split('demographicNo=') [1] 
+vPath = ('https://' + location.host + '/' + firstElement + '/')
+var myParam = location.search.split('demographicNo=') [1]
 var res = myParam.indexOf('&')
 var demo_no = myParam.substring(0, res)
 var measureArray = [
@@ -20,7 +28,7 @@ var measureDateArray = [
 function getMeasures(measure) {
   xmlhttp = new XMLHttpRequest();
   var pathArray = window.location.pathname.split('/');
-  var newURL = vPath + 'billing/CA/BC/billStatus.jsp?lastName=' + '' + '&firstName=' + '' + '&filterPatient=true&demographicNo=' + demo_no 
+  var newURL = vPath + 'billing/CA/BC/billStatus.jsp?lastName=' + '' + '&firstName=' + '' + '&filterPatient=true&demographicNo=' + demo_no  
   // window.open(newURL)
   xmlhttp.onreadystatechange = function () {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -37,7 +45,6 @@ function getMeasures(measure) {
         var mycode = y.substring(38, 43)        
         //alert(mycode)
         measureArray[i] = mycode;
-      
         i = i + 1;
       }      
       //alert(measureArray.length)
@@ -57,24 +64,30 @@ function getMeasures(measure) {
   xmlhttp.send();
 }
 getMeasures()
+var d = new Date();
+var currentyear = d.getFullYear()
 var z = measureArray.indexOf('14033')
 if (z > - 1) {
   var d = new Date(measureDateArray[z])
-  if (d.getFullYear() == d.getFullYear(Date())) {
-    alert('14033 billed this year')
+  if (d.getFullYear() == currentyear) {
+    fillfield('lightgreen', '14033 billed this year')    
+    //alert('14033 billed this year')
   } 
   else {
-    alert(measureArray[z] + ' last billed on ' + measureDateArray[z])
+    //alert(measureArray[z] + ' last billed on ' + measureDateArray[z])
+    fillfield('red', measureArray[z] + ' last billed on ' + measureDateArray[z])
   }
 }
 var z = measureArray.indexOf('14075')
 if (z > - 1) {
   var d = new Date(measureDateArray[z])
-  if (d.getFullYear() == d.getFullYear(Date())) {
-    alert('14075 billed this year')
+  if (d.getFullYear() == currentyear) {
+    fillfield('ligthgreen', '14075 billed this year')    
+    //alert('14075 billed this year')
   } 
   else {
-    alert(measureArray[z] + ' last billed on ' + measureDateArray[z])
+    //alert(measureArray[z] + ' last billed on ' + measureDateArray[z])
+    fillfield('red', measureArray[z] + ' last billed on ' + measureDateArray[z])
   }
 }
 /*
