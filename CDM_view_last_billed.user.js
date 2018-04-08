269c4627e64cfeaedf119807bd5a67504fd69cef
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2018-04-07 17:07:50 -0700

    Update CDM_view_last_billed.user.js

--- CDM_view_last_billed.user.js
+++ CDM_view_last_billed.user.js
@@ -10,13 +10,13 @@
 function fillfield(data1, data2) {
   $('#keyword').css('background-color', data1);
   $('#keyword').val(data2)
-  $('#keyword').click(function(){
-  $('#keyword').css('background-color', "white")
-  $('#keyword').val("")
-}); 
+  $('#keyword').click(function () {
+    $('#keyword').css('background-color', 'white')
+    $('#keyword').val('')
+  });
 }
 var elements = (window.location.pathname.split('/', 2))
-firstElement = (elements.slice(1)) //alert(firstElement)
+firstElement = (elements.slice(1))
 vPath = ('https://' + location.host + '/' + firstElement + '/')
 var myParam = location.search.split('demographicNo=') [1]
 var res = myParam.indexOf('&')
@@ -28,8 +28,7 @@ var measureDateArray = [
 function getMeasures(measure) {
   xmlhttp = new XMLHttpRequest();
   var pathArray = window.location.pathname.split('/');
-  var newURL = vPath + 'billing/CA/BC/billStatus.jsp?lastName=' + '' + '&firstName=' + '' + '&filterPatient=true&demographicNo=' + demo_no  
-  // window.open(newURL)
+  var newURL = vPath + 'billing/CA/BC/billStatus.jsp?lastName=' + '' + '&firstName=' + '' + '&filterPatient=true&demographicNo=' + demo_no
   xmlhttp.onreadystatechange = function () {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       var str = xmlhttp.responseText; //local variable
@@ -39,16 +38,12 @@ function getMeasures(measure) {
       var myRe = /<td align="center" class="bCellData" >[0-9]{5}<\/td>\s*/g; //for CDM code 
       var myArray
       var i = 0;
-      //while(i<14){
       while ((myArray = myRe.exec(str)) !== null) {
         y = myArray.toString()
-        var mycode = y.substring(38, 43)        
-        //alert(mycode)
+        var mycode = y.substring(38, 43)
         measureArray[i] = mycode;
         i = i + 1;
-      }      
-      //alert(measureArray.length)
-
+      }
       var myRe = /<td align="center" class="bCellData" >([0-9,-]+)<\/td>\s*<td align="center" class="bCellData" >/g; //for date
       var myArray;
       var i = 0;
@@ -63,33 +58,33 @@ function getMeasures(measure) {
   xmlhttp.open('GET', newURL, false);
   xmlhttp.send();
 }
-getMeasures()
-var d = new Date();
-var currentyear = d.getFullYear()
-var z = measureArray.indexOf('14033')
-if (z > - 1) {
-  var d = new Date(measureDateArray[z])
-  if (d.getFullYear() == currentyear) {
-    fillfield('lightgreen', '14033 billed this year')    
-    //alert('14033 billed this year')
-  } 
-  else {
-    //alert(measureArray[z] + ' last billed on ' + measureDateArray[z])
-    fillfield('red', measureArray[z] + ' last billed on ' + measureDateArray[z])
+$(document).ready(function () {
+  getMeasures()
+  var d = new Date();
+  var currentyear = d.getFullYear()
+  var z = measureArray.indexOf('14033')
+  if (z > - 1) {
+    var d = new Date(measureDateArray[z])
+    if (d.getFullYear() == currentyear) {
+      fillfield('lightgreen', '14033 billed this year') //alert('14033 billed this year')
+    } 
+    else {
+      //alert(measureArray[z] + ' last billed on ' + measureDateArray[z])
+      fillfield('red', measureArray[z] + ' last billed on ' + measureDateArray[z])
+    }
   }
-}
-var z = measureArray.indexOf('14075')
-if (z > - 1) {
-  var d = new Date(measureDateArray[z])
-  if (d.getFullYear() == currentyear) {
-    fillfield('ligthgreen', '14075 billed this year')    
-    //alert('14075 billed this year')
-  } 
-  else {
-    //alert(measureArray[z] + ' last billed on ' + measureDateArray[z])
-    fillfield('red', measureArray[z] + ' last billed on ' + measureDateArray[z])
+  var z = measureArray.indexOf('14075')
+  if (z > - 1) {
+    var d = new Date(measureDateArray[z])
+    if (d.getFullYear() == currentyear) {
+      fillfield('ligthgreen', '14075 billed this year') //alert('14075 billed this year')
+    } 
+    else {
+      //alert(measureArray[z] + ' last billed on ' + measureDateArray[z])
+      fillfield('red', measureArray[z] + ' last billed on ' + measureDateArray[z])
+    }
   }
-}
+});
 /*
 function getAllIndexes(arr, val) {
     var indexes = [], i = -1;
