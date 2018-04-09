18f82679b009fb9ecb68afe00b57ca714a026645
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2018-04-09 02:04:46 -0700

    Update mypagescraper.user.js

--- mypagescraper.user.js
+++ mypagescraper.user.js
@@ -11,51 +11,55 @@ firstElement = (elements.slice(1))
 vPath = ('https://' + location.host + '/' + firstElement + '/')
 var myParam = location.search.split('demographicNo=') [1]
 var res = myParam.indexOf('&')
-var demo_no = myParam.substring(0, res)//alert(demo_no)
+var demo_no = myParam.substring(0, res) 
+//alert(demo_no)
 var measureArray = [
 ];
 var measureDateArray = [
 ];
-
-//var demoArray["phone","demo_cell","email"]
-//for(i=0;i < demoArray.length;i++){
-
+var demoArray = [
+  'phone',
+  'demo_cell',
+  'email'
+]//var replace = "regex";
+//var re = new RegExp(replace,"g");
 function getMeasures(measure) {
   xmlhttp = new XMLHttpRequest();
   var pathArray = window.location.pathname.split('/');
   var newURL = vPath + 'demographic/demographiccontrol.jsp?demographic_no=' + demo_no + '&displaymode=edit&dboperation=search_detail'
   xmlhttp.onreadystatechange = function () {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      var str = xmlhttp.responseText.replace(/\s/g,'')
+      var str = xmlhttp.responseText.replace(/\s/g, '')      
       //alert(str)
       if (!str) {
         return;
-      }         
-     //var myRe = /<inputtype="text"name="demo_cell".*\s*/g;
-      var myRe = /<inputtype="text"name="email".*\s*/g;
-     //var myRe = /<inputtype="text"name="phone".*\s*/g;
-     //alert(myRe.exec(str))
+      }
+      var myReString = '<inputtype="text"name="' + measure + '".*/s*';
+      var myRe = new RegExp(myReString, 'g');
+      //var myRe = /<inputtype="text"name="phone".*\s*/g;   
       var myArray
       var i = 0;
-      while ((myArray = myRe.exec(str)) !== null) {    
-        y = myArray.toString()
+      while ((myArray = myRe.exec(str)) !== null) {
+        y = myArray.toString()        
         //alert(y)
         var z = y.indexOf('value=')
-        var mycode = y.substring(z+7)
+        var mycode = y.substring(z + 7)        
         //alert(mycode)
         var mycode2 = mycode.indexOf('">')
-
-        var mycode3= mycode.substring(mycode+8,mycode2) 
-        alert(mycode3)
+        var mycode3 = mycode.substring(mycode + 8, mycode2)
+        alert(measure + ' is ' + mycode3)
         measureArray[i] = mycode3;
         i = i + 1;
       }
-    }       
-  }
+    }
+  }  
   //alert(measureArray)
+
   xmlhttp.open('GET', newURL, false);
   xmlhttp.send();
 }
 $(document).ready(function () {
-  getMeasures()
+  for (i = 0; i < demoArray.length; i++) {
+    getMeasures(demoArray[i]);
+  }
 })
