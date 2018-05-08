957521d342d6ab91f920c3ee18aca00c6d01c3b9
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2018-05-08 08:18:30 -0700

    Update mypagescraper.user.js

--- mypagescraper.user.js
+++ mypagescraper.user.js
@@ -11,81 +11,59 @@ firstElement = (elements.slice(1))
 vPath = ('https://' + location.host + '/' + firstElement + '/')
 var myParam = location.search.split('demographicNo=') [1]
 var res = myParam.indexOf('&')
-var demo_no = myParam.substring(0, res) 
-//alert(demo_no)
-var measureArray = [
-];
-var measureDateArray = [
-];
+var demo_no = myParam.substring(0, res)
 var demoArray = [
   'CellPhone',
   'Email',
   'Phone(H)',
   'Address',
-  'City'
+  'City',
+  'Postal',
+  'Age',
+  'HealthIns.#'
 ]
-var Email="test"
+var demoArrayVal = [
+]
 function getMeasures(measure) {
   xmlhttp = new XMLHttpRequest();
   var pathArray = window.location.pathname.split('/');
   var newURL = vPath + 'demographic/demographiccontrol.jsp?demographic_no=' + demo_no + '&displaymode=edit&dboperation=search_detail'
   xmlhttp.onreadystatechange = function () {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      var str = xmlhttp.responseText.replace(/\s/g, '') 
-      //alert(str)
+      //alert(xmlhttp.responseText)
+      var str = xmlhttp.responseText.replace(/\s/g, '')
       if (!str) {
         return;
       }
-      //var myRe = /<inputtype="text"name="phone".*\s*/g;   
-      //var myRe = /<li><spanclass="label">CellPhone:<\/span><spanclass="info">.*\s*/g;     
-      var myReString = '<li><spanclass="label">' + measure + ':<\/span><spanclass="info">.*/s*'
-      //var myReString = '<inputtype="text"name="' + measure + '".*/s*';
-      //alert(myReString)
+      var myReString = '<li><spanclass="label">' + measure + ':</span><spanclass="info">.*/s*'
       var myRe = new RegExp(myReString, 'g');
-            
-      //alert(myRe)
       var myArray
-      var i = 0;
       while ((myArray = myRe.exec(str)) !== null) {
-        y = myArray.toString()        
-        //alert(y)  
-        //var z = y.indexOf('value=')
+        y = myArray.toString()
+       // alert(y)
         var z = y.indexOf('info')
-        //var mycode = y.substring(z + 7)
         var mycode = y.substring(z + 6)
-        //alert(mycode)
-        //var mycode2 = mycode.indexOf('">')
         var mycode2 = mycode.indexOf('</span>')
-        //var mycode3 = mycode.substring(mycode + 8, mycode2)
-        var mycode3 = mycode.substring(mycode + 9, mycode2)
-       // alert(measure + ' is ' + mycode3)
-       // eval(measure) = mycode3
-    //eval(measure + " = " + "'" + mycode3 + "'");//maps the measure to a vaiable called measure
-    //alert(Email)
-   // alert(CellPhone)
-   // alert(Address)    
-        measureArray[i] = mycode3;
-        i = i + 1;
+        var mycode3 = mycode.substring(mycode + 9, mycode2)        
+        //alert(j+measure + ' is ' + mycode3)
+        demoArrayVal[j] = mycode3
       }
     }
-  }  
- 
-
+  }
   xmlhttp.open('GET', newURL, false);
   xmlhttp.send();
 }
 $(document).ready(function () {
-  for (i = 0; i < demoArray.length; i++) {
-    getMeasures(demoArray[i]);
+  for (j = 0; j < demoArray.length; j++) {
+    getMeasures(demoArray[j]);
   }
+var header = document.getElementById('encounterHeader');
+var headerExtra1 = 'Add: '
+var headerExtra3 = 'File#: '
+var headerExtra4 = 'HC: '
+header.innerHTML += ('<br>' + headerExtra1.bold() + demoArrayVal[3]+","+ demoArrayVal[4] + ',' + demoArrayVal[5] + ',' + demoArrayVal[6] + ' '
+ + ' ' + headerExtra3.bold() + demo_no + ' ' + headerExtra4.bold()
++ demoArrayVal[7] + '   email: ' + demoArrayVal[1] + '   '
++ '<a href="mailto:' + demoArrayVal[1] + '?Subject=Confidential medical information" target="_blank">Send Mail</a>'
+);
 })
-    var header = document.getElementById('encounterHeader');
-    var headerExtra1 = 'Add: '
-    var headerExtra2 = 'DOB: '
-    var headerExtra3 = 'File#: '
-    var headerExtra4 = 'HC: '
-    header.innerHTML += ('<br>' + headerExtra1.bold() + 'Address' + ',' + 'City' + ',' + 'postalCode' + 'postalCode2' + ' '
-    + headerExtra2.bold() + 'DOB' + ' ' + headerExtra3.bold() + 'demoNo' + ' ' + headerExtra4.bold()
-    + 'HCN' + 'HCVC' + '   email: ' + Email + '   '
-    + '<a href="mailto:' + Email + '?Subject=Confidential medical information" target="_blank">Send Mail</a>'
-    );
