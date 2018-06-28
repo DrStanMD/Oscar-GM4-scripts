c8f7546c8a46e9604a2df1e0c979397ab0cd0721
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2018-06-27 18:07:46 -0700

    Update Stickynote.user.js

--- Stickynote.user.js
+++ Stickynote.user.js
@@ -17,16 +17,17 @@ function getMeasures(measure) {
   var newURL = vPath + 'oscarMessenger/DisplayMessages.do'
   xmlhttp.onreadystatechange = function () {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      var str = xmlhttp.responseText      //alert(str)
+      var str = xmlhttp.responseText
+      alert(str)
       var str = xmlhttp.responseText.replace(/\s/g, '')      //alert(str)
       //alert(str.indexOf("StickyNote", str.indexOf("StickyNote")+2));
       if (str.indexOf('StickyNote') > - 1) {
-        var x = str.indexOf('StickyNote')        //alert(x)
-        str = str.slice(x - 60)        //alert(str)
+        var x = str.indexOf('StickyNote') //alert(x)
+        str = str.slice(x - 60) //alert(str)
         var start = str.indexOf('messageID=')
         var end = str.indexOf('&boxType')
-        mymsgId = str.substring(start + 10, end)        
-         if (confirm('New incoming StickyNote.  Read Now?')) {
+        mymsgId = str.substring(start + 10, end)
+        if (confirm('New incoming StickyNote.  Read Now?')) {
           //var myWindow = window.open(newURL, '', 'toolbar=no,menubar=no,dialog=no,width=800,height=600');
           var myWindow = window.open(vPath + 'oscarMessenger/ViewMessage.do?messageID=' + mymsgId, '', 'toolbar=no,menubar=no,dialog=no,width=800,height=600');
         } else {
@@ -43,17 +44,13 @@ function getMeasures(measure) {
 }
 getMeasures('X')
 xmlhttp = new XMLHttpRequest();
-xmlhttp.open('GET', vPath+'oscarMessenger/ViewMessage.do?messageID='+mymsgId,false);
+xmlhttp.open('GET', vPath + 'oscarMessenger/ViewMessage.do?messageID=' + mymsgId, false);
 xmlhttp.send();
-var str2 = xmlhttp.responseText.replace(/\s/g, '')
-//alert(str2)
-var y =str2.indexOf('textareaid="msgBody"name="Message"')
+var str2 = xmlhttp.responseText.replace(/\s/g, '')//alert(str2)
+var y = str2.indexOf('textareaid="msgBody"name="Message"')
 str2 = str2.slice(y + 79)
-var z = str2.indexOf("</textarea><br>")
-alert(str2.slice(0,z))
-//newWindow = window.open(vPath+'eform/efmshowform_data.jsp?fid=430', '', 'toolbar=no,menubar=no,dialog=no,width=400,height=200,top=0, left=0')
-
-
+var z = str2.indexOf('</textarea><br>')
+alert(str2.slice(0, z))//newWindow = window.open(vPath+'eform/efmshowform_data.jsp?fid=430', '', 'toolbar=no,menubar=no,dialog=no,width=400,height=200,top=0, left=0')
 setInterval(function () {
   //getMeasures('X');
 }, 30000);
