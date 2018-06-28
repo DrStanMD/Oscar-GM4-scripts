eea04d988b729bfacdfc7562455200692ef61b66
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2018-06-27 17:57:37 -0700

    Update Stickynote.user.js

--- Stickynote.user.js
+++ Stickynote.user.js
@@ -26,8 +26,7 @@ function getMeasures(measure) {
         var start = str.indexOf('messageID=')
         var end = str.indexOf('&boxType')
         mymsgId = str.substring(start + 10, end)        
-        //newWindow = window.open(vPath+'eform/efmshowform_data.jsp?fid=430', '', 'toolbar=no,menubar=no,dialog=no,width=400,height=200,top=0, left=0')
-        if (confirm('New incoming StickyNote.  Read Now?')) {
+         if (confirm('New incoming StickyNote.  Read Now?')) {
           //var myWindow = window.open(newURL, '', 'toolbar=no,menubar=no,dialog=no,width=800,height=600');
           var myWindow = window.open(vPath + 'oscarMessenger/ViewMessage.do?messageID=' + mymsgId, '', 'toolbar=no,menubar=no,dialog=no,width=800,height=600');
         } else {
@@ -42,12 +41,19 @@ function getMeasures(measure) {
   xmlhttp.open('GET', newURL, false);
   xmlhttp.send();
 }
-getMeasures('X')//alert(mymsgId)
-//window.open(vPath+'oscarMessenger/ViewMessage.do?messageID='+mymsgId)
-//xmlhttp.open('GET', vPath+'oscarMessenger/ViewMessage.do?messageID=6604');
-//  xmlhttp.send();
-//  var str2 = xmlhttp.responseText.replace(/\s/g, '')
-// alert(str2.indexOf("test2"))
+getMeasures('X')
+xmlhttp = new XMLHttpRequest();
+xmlhttp.open('GET', vPath+'oscarMessenger/ViewMessage.do?messageID='+mymsgId,false);
+xmlhttp.send();
+var str2 = xmlhttp.responseText.replace(/\s/g, '')
+//alert(str2)
+var y =str2.indexOf('textareaid="msgBody"name="Message"')
+str2 = str2.slice(y + 79)
+var z = str2.indexOf("</textarea><br>")
+alert(str2.slice(0,z))
+//newWindow = window.open(vPath+'eform/efmshowform_data.jsp?fid=430', '', 'toolbar=no,menubar=no,dialog=no,width=400,height=200,top=0, left=0')
+
+
 setInterval(function () {
   //getMeasures('X');
 }, 30000);
