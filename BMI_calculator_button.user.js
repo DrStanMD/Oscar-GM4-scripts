6dfaea895a61e64a220ab2387a485ee6e195cc9f
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2018-10-12 15:13:28 -0700

    Update BMI_calculator_button.user.js

--- BMI_calculator_button.user.js
+++ BMI_calculator_button.user.js
@@ -1,19 +1,19 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name        BMI calculator button
 // @namespace   Stanscript
 // @description Calculates BMI, Converts to lbs, ins.
 // @include     *oscarMeasurements/SetupMeasurements.do?groupName=Vitals*
 // @require   http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
-// @version     1
+// @version     15.1
 // @grant       none
 // ==/UserScript==
 window.resizeTo(1050, 700);
 $('input[name=\'value(inputValue-7)\']').change(ButtonFunction1)
 $('input[name=\'value(inputValue-2)\']').change(ButtonFunction1)
-$('input[name=\'value(inputValue-1)\']').css('background-color', 'lightgreen');
-$('input[name=\'value(inputValue-7)\']').css('background-color', 'yellow');
-$('input[name=\'value(inputValue-2)\']').css('background-color', 'yellow');
-$('input[name=\'value(inputValue-3)\']').css('background-color', 'lightgreen');
+$('input[name=\'value(inputValue-0)\']').css('background-color', 'lightgreen');
+$('input[name=\'value(inputValue-1)\']').css('background-color', 'yellow');
+$('input[name=\'value(inputValue-2)\']').css('background-color', 'lightgreen');
+//$('input[name=\'value(inputValue-3)\']').css('background-color', 'lightgreen');
 /*
 var input1=document.createElement("input");
 input1.type="button";
@@ -23,10 +23,10 @@ input1.setAttribute("style", "font-size:16px;position:fixed;top:100px;left:600px
 document.body.appendChild(input1); 
 */
 function ButtonFunction1() {
-  vWT = $('input[name=\'value(inputValue-7)\']').val();
+  vWT = $('input[name=\'value(inputValue-1)\']').val();
   if (vWT) {
     vWTlb = (Number(vWT) * 2.2).toFixed(1)
-    $('input[name=\'value(comments-7)\']').val(' (' + vWTlb + ' lbs)');
+    $('input[name=\'value(comments-1)\']').val(' (' + vWTlb + ' lbs)');
   }
   vHT = $('input[name=\'value(inputValue-2)\']').val();
   if (vHT) {
@@ -39,10 +39,9 @@ function ButtonFunction1() {
     vBMI = vWT / (vHT * 0.01 * vHT * 0.01)
   }
   if (vBMI) {
-    $('input[name=\'value(inputValue-0)\']').val(vBMI.toFixed(1));
+    $('input[name=\'value(inputValue-6)\']').val(vBMI.toFixed(1));
     vIW = 25 * (vHT * vHT * 0.0001)
-    $('input[name=\'value(comments-0)\']').val('Ideal weight is ' + vIW.toFixed() + ' kg. (' + (vIW * 2.2).toFixed() + ' lbs.)')
-    //alert('Ideal weight is ' + vIW.toFixed() + ' kg. (' + (vIW * 2.2).toFixed() + ' lbs.)')
+    $('input[name=\'value(comments-6)\']').val('Ideal weight is ' + vIW.toFixed() + ' kg. (' + (vIW * 2.2).toFixed() + ' lbs.)')    //alert('Ideal weight is ' + vIW.toFixed() + ' kg. (' + (vIW * 2.2).toFixed() + ' lbs.)')
   }
 }
-$('tr.data:nth-child(4) > td:nth-child(3) > input:nth-child(1)').focus()
+$('tr.data:nth-child(3) > td:nth-child(3) > input:nth-child(1)').focus()
