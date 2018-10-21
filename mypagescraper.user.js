9f649a9fdb52f35c3178fffdbdd970aa06927a3d
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2018-10-21 10:26:45 -0700

    Update mypagescraper.user.js

--- mypagescraper.user.js
+++ mypagescraper.user.js
@@ -5,7 +5,7 @@
 // @include     */casemgmt/forward.jsp?action=view&demographic*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
 // @grant       none
-// @version 15.1
+// @version 15.2
 // ==/UserScript==
 var elements = (window.location.pathname.split('/', 2))
 firstElement = (elements.slice(1))
@@ -14,14 +14,14 @@ var myParam = location.search.split('demographicNo=') [1]
 var res = myParam.indexOf('&')
 var demo_no = myParam.substring(0, res)
 var demoArray = [
-  'CellPhone',
+  'Cell Phone',
   'Email',
   'Phone(H)',
   'Address',
   'City',
   'Postal',
   'Age',
-  'HealthIns.#'
+  'Health Ins'
 ]
 var demoArrayVal = [
 ]
@@ -32,16 +32,19 @@ function getMeasures(measure) {
   xmlhttp.onreadystatechange = function () {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       //alert(xmlhttp.responseText)
-      var str = xmlhttp.responseText.replace(/\s/g, '')
+      //var str = xmlhttp.responseText.replace(/\s/g, '')
+      var str = xmlhttp.responseText
       if (!str) {
         return;
       }
       //var myReString = '<li><spanclass="label">' + measure + ':</span><spanclass="info">.*/s*'
-      var myReString = '<spanclass="label">'  + measure + '.*/s*'
+      //var myReString = '<spanclass="label">' + measure + '.*/s*'
+      var myReString = '<span class="label">'  + measure +  '(.|[\n])*'
       var myRe = new RegExp(myReString, 'g');
       var myArray
       while ((myArray = myRe.exec(str)) !== null) {
-        y = myArray.toString() // alert(y)
+        y = myArray.toString() 
+        //alert(y)
         var z = y.indexOf('info')
         var mycode = y.substring(z + 6)
         var mycode2 = mycode.indexOf('</span>')
