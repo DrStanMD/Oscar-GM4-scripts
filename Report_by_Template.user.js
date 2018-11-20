ca6cd1a0477bf7e3a14f0e8c0d7586a0b0bc8704
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2018-11-19 22:58:50 -0800

    Update Report_by_Template.user.js

--- Report_by_Template.user.js
+++ Report_by_Template.user.js
@@ -3,16 +3,15 @@
 // @namespace   Stanscript
 // @include     *oscarReport/reportByTemplate*
 // @description RBT alphabetical, Pass parameter to RBT
-// @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
+// @require   https://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js
 // @grant       none
-// @version     1.5
+// @version     15.1
 // ==/UserScript==
 //alert("HI")
 //========Get Path============
 var elements = (window.location.pathname.split('/', 2))
 firstElement = (elements.slice(1))
-vPath = ('https://' + location.host + '/' + firstElement + '/')
-//=====Get Parameters============
+vPath = ('https://' + location.host + '/' + firstElement + '/')//=====Get Parameters============
 var params = {
 };
 if (location.search) {
@@ -23,8 +22,7 @@ if (location.search) {
     params[nv[0]] = nv[1] || true;
   }
 }
-$('#enclosingCol0 > input:nth-child(1)').val(params.demographic_no)
-//******************************************************************************
+$('#enclosingCol0 > input:nth-child(1)').val(params.demographic_no)//******************************************************************************
 $('.MainTableLeftColumn').append('<td>Favorites List - for future use</td>');
 d = $('div.templatelist').html()
 var parts = d.split('</li>');
@@ -48,20 +46,16 @@ for (i = 0; i < myArray.length - 1; i++) {
   startat = (myNewArray[i].indexOf('<a'))
   myNewArray[i] = myNewArray[i].substring(startat)
 }
-newlist = '<br><a href=' + vPath + 'oscarReport/reportByTemplate/addEditTemplate.jsp style=\'color: #226d55; font-size: 10px;\'>Add Template</a><br>'
-//newlist=""
+newlist = '<br><a href=' + vPath + 'oscarReport/reportByTemplate/addEditTemplate.jsp style=\'color: #226d55; font-size: 10px;\'>Add Template</a><br>'//newlist=""
 for (var i = 0; i < myNewArray.length; i++) {
   //**************************************
   //**************NUMBERED LIST:
-  newlist = newlist + '<br>' + (i + 1) + '. ' + myNewArray[i]
-  //*************WITHOUT NUMBERS
+  newlist = newlist + '<br>' + (i + 1) + '. ' + myNewArray[i]  //*************WITHOUT NUMBERS
   //newlist = newlist + "<br>"+myNewArray[i]
-}
-//**********REMOVE EXISTING LIST:
+}//**********REMOVE EXISTING LIST:
 
-$('div.templatelist').html('<a href=' + vPath + 'oscarReport/reportByTemplate/addEditTemplate.jsp style=\'color: #226d55; font-size: 10px;\'>Add Template</a><br>')
-//**********APPPEND TO BOTTOM:
-$('.MainTableBottomRowRightColumn').html(newlist)
-//*********REPLACE EXISTING LIST:
+$('div.templatelist').html('<a href=' + vPath + 'oscarReport/reportByTemplate/addEditTemplate.jsp style=\'color: #226d55; font-size: 10px;\'>Add Template</a><br>')//**********APPPEND TO BOTTOM:
+$('.MainTableBottomRowRightColumn').html(newlist)//*********REPLACE EXISTING LIST:
 //$('div.templatelist') .html(newlist)
-//$('input[value="Run Query"]').click();
+var x = document.getElementsByName('submitButton');
+$(x[0]).click()
