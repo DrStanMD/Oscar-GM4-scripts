bb64b79ea39fbb294357010b1715325a585cbb2d
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2019-02-06 09:13:41 -0800

    Update EChart_KeyboardShortcuts.user.js

--- EChart_KeyboardShortcuts.user.js
+++ EChart_KeyboardShortcuts.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name           EChart_KeyboardShortcuts
 // @namespace      oscar/StansScripts
-// @description Various Echart shortcut buttons (Shift+ E,V,Z,X,I,F,K,S,M,N,P,0,1,2,3,4,5,7). Set your own Measurement groupName and default population text.
+// @description Various Echart shortcut buttons (Alt+ e,v,z,x,i,f,k,s,m,n,p,0,1,2,3,4,5,7). Set your own Measurement groupName and default population text.
 // @include        */casemgmt/forward.jsp?action=view&*
-// @version 15.1
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
 // ==/UserScript==
 var elements = (window.location.pathname.split('/', 2))
-firstElement = (elements.slice(1))//alert(firstElement)
-vPath = ('https://' + location.host + '/' + firstElement)//alert(vPath)
+firstElement = (elements.slice(1))
+//alert(firstElement)
+vPath = ('https://' + location.host + '/' + firstElement)
+//alert(vPath)
 //vPath = ("https://" + location.hostname + "/")
 window.addEventListener('dblclick', function () {
   //alert("HELLO WORLD")
@@ -26,67 +27,70 @@ window.addEventListener('keypress', function (theEvent) {
   var theShiftKey = theEvent.shiftKey;
   //var theDownKey= theEvent.PgDnKey;
   switch (true) {
-    case theShiftKey && theKey == 'E': //save, sign and bill
+    case theAltKey && theKey == 'e': //save, sign and bill
       //var theTarget = document.evaluate("id('save')/span/input[contains(@src,'dollar-sign-icon.png')]",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
       //theTarget.click();
       $('#save > span:nth-child(1) > input:nth-child(5)').click()
       break;
-    case theShiftKey && theKey == 'V': //sign & exit
+    case theAltKey && theKey == 'v': //sign & exit
       $('#signSaveImg').click()
       break;
-    case theShiftKey && theKey == 'Z': //open BP Pulse Weight Height measurements
+    case theAltKey && theKey == 'z': //open BP Pulse Weight Height measurements
       window.open(vPath + '/oscarEncounter/oscarMeasurements/SetupMeasurements.do?groupName=Vitals', 'VitalsWindow', 'width=1000,height=500')
       break;
-    case theShiftKey && theKey == 'X': //open Screening Measurements
+    case theAltKey && theKey == 'x': //open Screening Measurements
       window.open(vPath + '/oscarEncounter/oscarMeasurements/SetupMeasurements.do?groupName=Screening%20Procedures', 'Screening%20ProceduresWindow', 'width=1000,height=700')
       break;
-    case theShiftKey && theKey == 'I': //open Injections/Allergy shots
+    case theAltKey && theKey == 'i': //open Injections/Allergy shots
       window.open(vPath + '/oscarEncounter/oscarMeasurements/SetupMeasurements.do?groupName=Allergy Shots')
       break;
-    case theShiftKey && theKey == 'F': //open eForms & Lab forms
+    case theAltKey && theKey == 'f': //open eForms & Lab forms
       $('#menuTitleeforms > h3:nth-child(1) > a:nth-child(1)').click()
       break;
-    case theShiftKey && theKey == 'S': //save note
+    case theAltKey && theKey == 's': //save note
       var theTarget = document.evaluate('id(\'frmIssueNotes\')/span/input[contains(@src,\'note-save.png\')]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
       theTarget.click();
       break;
-    case theShiftKey && theKey == '!': //new social history
+    case theAltKey && theKey == '1': //new social history
       vtext = 'Smoker: Non.\nAlcohol: minimal.\nExercises: \nMarried: Y \nKids  \nOccupation: \n'
       $('#divR1I1 > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
       $('#noteEditTxt').val(vtext)
       break;
-    case theShiftKey && theKey == '@': //new medical history
+    case theAltKey && theKey == '2': //new medical history
       vtext = '\n\nSURGICAL HISTORY:\n'
       $('#divR1I2 > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
       $('#noteEditTxt').val(vtext)
       break;
-    case theShiftKey && theKey == '#': //new Family History
+    case theAltKey && theKey == '3': //new Family History
       vtext = 'Diabetes: No.\nCAD: No.\nHypertension: No.\nStroke: No.\nCancer: No.\nDementia: No.\n'
       $('#FamHistory > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
       $('#noteEditTxt').val(vtext)
       break;
-    case theShiftKey && theKey == '$': //new ongoing concerns
-      $('#divR2I1 > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()      //$('#noteEditTxt').val(vtext)
+    case theAltKey && theKey == '4': //new ongoing concerns
+      $('#divR2I1 > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
+      //$('#noteEditTxt').val(vtext)
       break;
-    case theShiftKey && theKey == '%': //new reminder
-      $('#divR2I2 > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()      //$('#noteEditTxt').val(vtext)
+    case theAltKey && theKey == '5': //new reminder
+      $('#divR2I2 > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
+      //$('#noteEditTxt').val(vtext)
       break;
-    case theShiftKey && theKey == '0': //expand measurements
-      $('#imgmeasurements5').click()      //$('#noteEditTxt').val(vtext)
+    case theAltKey && theKey == '0': //expand measurements
+      $('#imgmeasurements5').click()
+      //$('#noteEditTxt').val(vtext)
       break;
-    case theShiftKey && theKey == 'M': //Add medications
+    case theAltKey && theKey == 'm': //Add medications
       $('#Rx > div:nth-child(3) > h3:nth-child(1) > a:nth-child(1)').click()
       break;
-    case theShiftKey && theKey == 'P': //Add preventions
+    case theAltKey && theKey == 'p': //Add preventions
       $('#preventions > div:nth-child(3) > h3:nth-child(1) > a:nth-child(1)').click()
       break;
-    case theShiftKey && theKey == 'N': //Add preventions
+    case theAltKey && theKey == 'n': //Add preventions
       $('#newNoteImg').click()
       break;
-    case theShiftKey && theKey == 'K': //Open Scratch Pad
+    case theAltKey && theKey == 'k': //Open Scratch Pad
       window.open(vPath + '/scratch/index.jsp')
       break;
-    case theShiftKey && theKey == '&': //change message colour to red
+    case theAltKey && theKey == '7': //change message colour to red
       oldvalue = $('#noteEditTxt').val()
       newvalue = '<div style=\'color: Red\'>' + oldvalue + '</div>'
       $('#noteEditTxt').val(newvalue)
