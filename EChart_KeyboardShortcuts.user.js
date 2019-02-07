f341c78f2bc2438de709623cb2efff0bafdd8ecf
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2019-02-06 17:51:20 -0800

    Update EChart_KeyboardShortcuts.user.js

--- EChart_KeyboardShortcuts.user.js
+++ EChart_KeyboardShortcuts.user.js
@@ -1,105 +1,81 @@
 // ==UserScript==
 // @name           EChart_KeyboardShortcuts
 // @namespace      oscar/StansScripts
+// @version 15.1
 // @description Various Echart shortcut buttons (Alt+ e,v,z,x,i,f,k,s,m,n,p,0,1,2,3,4,5,7). Set your own Measurement groupName and default population text.
 // @include        */casemgmt/forward.jsp?action=view&*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
 // ==/UserScript==
+
 var elements = (window.location.pathname.split('/', 2))
 firstElement = (elements.slice(1))
-//alert(firstElement)
 vPath = ('https://' + location.host + '/' + firstElement)
-//alert(vPath)
-//vPath = ("https://" + location.hostname + "/")
 window.addEventListener('dblclick', function () {
-  //alert("HELLO WORLD")
   oldvalue = $('#noteEditTxt').val()
   newvalue = '<div style=\'color: Red\'>' + oldvalue + '</div>'
   $('#noteEditTxt').val(newvalue)
 });
-window.addEventListener('keypress', function (theEvent) {
-  //theEvent.stopPropagation();
-  //theEvent.preventDefault();
-  var theKeyCode = theEvent.charCode; // || event.which;
-  var theKey = String.fromCharCode(theKeyCode);
-  var theAltKey = theEvent.altKey;
-  var theCtrlKey = theEvent.ctrlKey;
-  var theShiftKey = theEvent.shiftKey;
-  //var theDownKey= theEvent.PgDnKey;
-  switch (true) {
-    case theAltKey && theKey == 'e': //save, sign and bill
+
+
+(function () {
+  document.addEventListener('keydown', function (e) {
+    
+   // alert(e.keyCode)
+    
+    if (e.keyCode == 90 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) { //z
+      window.open('https://secure56.junoemr.com/SDHurwitzInc//oscarEncounter/oscarMeasurements/SetupMeasurements.do?groupName=Vitals')
+    }
+    if (e.keyCode == 86 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) { //v
+      $('#signSaveImg').click()
+    }
+    if (e.keyCode == 75 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) { //k
+      window.open(vPath + '/scratch/index.jsp')
+    }
+    if (e.keyCode == 88 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) { //x
+      window.open(vPath + '/oscarEncounter/oscarMeasurements/SetupMeasurements.do?groupName=Screening%20Procedures', 'Screening%20ProceduresWindow', 'width=1000,height=700')
+    }
+    if (e.keyCode == 88 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) { //x
+      window.open(vPath + '/oscarEncounter/oscarMeasurements/SetupMeasurements.do?groupName=Screening%20Procedures', 'Screening%20ProceduresWindow', 'width=1000,height=700')
+    }
+    if (e.keyCode == 69 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) { //e
       //var theTarget = document.evaluate("id('save')/span/input[contains(@src,'dollar-sign-icon.png')]",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
       //theTarget.click();
       $('#save > span:nth-child(1) > input:nth-child(5)').click()
-      break;
-    case theAltKey && theKey == 'v': //sign & exit
-      $('#signSaveImg').click()
-      break;
-    case theAltKey && theKey == 'z': //open BP Pulse Weight Height measurements
-      window.open(vPath + '/oscarEncounter/oscarMeasurements/SetupMeasurements.do?groupName=Vitals', 'VitalsWindow', 'width=1000,height=500')
-      break;
-    case theAltKey && theKey == 'x': //open Screening Measurements
+    }
+    if (e.keyCode == 83 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) { //s
       window.open(vPath + '/oscarEncounter/oscarMeasurements/SetupMeasurements.do?groupName=Screening%20Procedures', 'Screening%20ProceduresWindow', 'width=1000,height=700')
-      break;
-    case theAltKey && theKey == 'i': //open Injections/Allergy shots
-      window.open(vPath + '/oscarEncounter/oscarMeasurements/SetupMeasurements.do?groupName=Allergy Shots')
-      break;
-    case theAltKey && theKey == 'f': //open eForms & Lab forms
-      $('#menuTitleeforms > h3:nth-child(1) > a:nth-child(1)').click()
-      break;
-    case theAltKey && theKey == 's': //save note
-      var theTarget = document.evaluate('id(\'frmIssueNotes\')/span/input[contains(@src,\'note-save.png\')]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-      theTarget.click();
-      break;
-    case theAltKey && theKey == '1': //new social history
+    }
+    if (e.keyCode == 49 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) { //1
       vtext = 'Smoker: Non.\nAlcohol: minimal.\nExercises: \nMarried: Y \nKids  \nOccupation: \n'
       $('#divR1I1 > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
       $('#noteEditTxt').val(vtext)
-      break;
-    case theAltKey && theKey == '2': //new medical history
+    }
+    if (e.keyCode == 50 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) { //2
       vtext = '\n\nSURGICAL HISTORY:\n'
       $('#divR1I2 > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
       $('#noteEditTxt').val(vtext)
-      break;
-    case theAltKey && theKey == '3': //new Family History
+    }
+    if (e.keyCode == 51 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) { //3
       vtext = 'Diabetes: No.\nCAD: No.\nHypertension: No.\nStroke: No.\nCancer: No.\nDementia: No.\n'
       $('#FamHistory > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
       $('#noteEditTxt').val(vtext)
-      break;
-    case theAltKey && theKey == '4': //new ongoing concerns
-      $('#divR2I1 > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
-      //$('#noteEditTxt').val(vtext)
-      break;
-    case theAltKey && theKey == '5': //new reminder
+    }
+    if (e.keyCode == 52 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) { //4
+      $('#divR2I1 > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click() //$('#noteEditTxt').val(vtext)
+    }
+    if (e.keyCode == 53 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) { //5
       $('#divR2I2 > div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').click()
-      //$('#noteEditTxt').val(vtext)
-      break;
-    case theAltKey && theKey == '0': //expand measurements
-      $('#imgmeasurements5').click()
-      //$('#noteEditTxt').val(vtext)
-      break;
-    case theAltKey && theKey == 'm': //Add medications
-      $('#Rx > div:nth-child(3) > h3:nth-child(1) > a:nth-child(1)').click()
-      break;
-    case theAltKey && theKey == 'p': //Add preventions
-      $('#preventions > div:nth-child(3) > h3:nth-child(1) > a:nth-child(1)').click()
-      break;
-    case theAltKey && theKey == 'n': //Add preventions
-      $('#newNoteImg').click()
-      break;
-    case theAltKey && theKey == 'k': //Open Scratch Pad
-      window.open(vPath + '/scratch/index.jsp')
-      break;
-    case theAltKey && theKey == '7': //change message colour to red
+    }
+    if (e.keyCode == 96 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) { //0
+  $('#imgeforms5').click()
+  $('#imgmeasurements5').click()
+  $('#imgdocs5').click()
+  $('#imglabs5').click()
+    }
+    if (e.keyCode == 55 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) { //7
       oldvalue = $('#noteEditTxt').val()
       newvalue = '<div style=\'color: Red\'>' + oldvalue + '</div>'
       $('#noteEditTxt').val(newvalue)
-      break;
-      /*
-		case theAltKey && theCtrlKey && theShiftKey && theKey=='':
-			//TO DO: The action to be performed for the above keyboard shortcut
-			break;
-		*/
-  }
-},
-true);
+    }
+  }, false);
+}) ();
