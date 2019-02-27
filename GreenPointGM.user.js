3ce5ec98f62971e91750e49d6147844f8d9b14c6
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2019-02-27 08:13:49 -0800

    Create GreenPointGM.user.js

---
+++ GreenPointGM.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name     Green Point
+// @version  1
+// @include *PacificRim/GreenPoint/1-94?Calendar*
+// @require   https://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js
+// @grant    none
+// ==/UserScript==
+
+
+/*
+https://reservation.pc.gc.ca/PacificRim/GreenPoint/1-94?Calendar
+
+#selResType - Frontcountry Camping
+
+#selArrMth - May
+#selArrDay - 9th
+#selNumNights - 4
+#MainContentPlaceHolder_LocationList - Pacific Rim
+#MainContentPlaceHolder_MapList - 1-94
+#selEquipmentSub - Small Tent
+#selPartySize - 2
+#selResource  -  39
+
+#reserveButton - 
+*/
