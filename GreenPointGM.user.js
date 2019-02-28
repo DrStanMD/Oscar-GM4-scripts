1d3f316a5cef25137ffc6b0f8de21cbde56e389a
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2019-02-27 21:35:26 -0800

    Update GreenPointGM.user.js

--- GreenPointGM.user.js
+++ GreenPointGM.user.js
@@ -1,26 +1,30 @@
 // ==UserScript==
 // @name     Green Point
 // @version  1
-// @include *PacificRim/GreenPoint/1-94?Calendar*
+// @include *PacificRim/GreenPoint*
 // @require   https://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js
 // @grant    none
 // ==/UserScript==
+//$('#selResType').css('background-color', 'yellow')
+$('#selResType').val('Frontcountry Camping')
+document.getElementById('selArrMth').selectedIndex = '3';
+$('#selArrMth').trigger('change');
+$('#selArrDay').val('9th')
+$('#selNumNights').val('2')
+$('#selNumNights').trigger('change');
+$('#selArrDay').css('background-color', 'yellow')
+$('#MainContentPlaceHolder_LocationList').val('Pacific Rim')
+$('#MainContentPlaceHolder_MapList').val('94')
+$('#selEquipmentSub').val('Small Tent')
+$('#selPartySize').val('2')
+$('#selResource').val('91')
+$('#selResource').trigger('change');
 
-$('#selResType').css('background-color', 'yellow')
+
+$(document).ready(function(){
+alert(document.getElementById('reserveButton').disabled)
+});
+
 
 /*
-https://reservation.pc.gc.ca/PacificRim/GreenPoint/1-94?Calendar
-
-#selResType - Frontcountry Camping
-
-#selArrMth - May
-#selArrDay - 9th
-#selNumNights - 4
-#MainContentPlaceHolder_LocationList - Pacific Rim
-#MainContentPlaceHolder_MapList - 1-94
-#selEquipmentSub - Small Tent
-#selPartySize - 2
-#selResource  -  39
-
-#reserveButton - 
-*/
+https://reservation.pc.gc.ca/PacificRim/GreenPoint/1-94
