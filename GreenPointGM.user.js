ea4c260f58ee9cc39da2a6deb70c1a15aa2bf692
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2019-02-28 05:53:52 -0800

    Update GreenPointGM.user.js

--- GreenPointGM.user.js
+++ GreenPointGM.user.js
@@ -6,12 +6,12 @@
 // @grant    none
 // ==/UserScript==
 //https://reservation.pc.gc.ca/PacificRim/GreenPoint/1-94
-d = new Date('May 27, 2019')
-alert(d)
+d = new Date('Apr 28, 2019')//alert(d)
 d.setDate(d.getDate() + 7);
-alert(d)
-cm = d.getMonth() - 1
-cd = d.getDate() - 1
+//alert(d)
+var cm = d.getMonth() - 1
+var cd = d.getDate() - 1
+var cs = 3
 var mysites = [
   '90',
   '1',
@@ -48,18 +48,28 @@ var mymonth = [
   'Nov',
   'Dec'
 ]
+var myday = [
+  'Sun',
+  'Mon',
+  'Tue',
+  'Wed',
+  'Thu',
+  'Fri',
+  'Sat'
+]
 function mychange() {
   var dm = new Date($('#selArrMth').val());
   var nm = mymonth[dm.getMonth()];
   //alert(nm)
   var dd = new Date($('#selArrDay').val());
   var nd = dd.getDate();
+  var ndow = myday[dd.getDay()];
   //alert(n)
   $(document).ready(function () {
     for (i = 0; i < mysites.length; i++) {
       var x = $('#rce_' + mysites[i]).html()
       if (x.indexOf('rce avail') > - 1) {
-        alert('Site ' + mysites[i] + ' is available from ' + nm + ' ' + nd + ' for ' + $('#selNumNights').val() + ' nights.')
+        alert('Site ' + mysites[i] + ' is available from ' + ndow + ', ' + nm + ' ' + nd + ' for ' + $('#selNumNights').val() + ' nights.')
       }
     }
   });
@@ -69,17 +79,14 @@ document.getElementById('selArrMth').selectedIndex = cm;
 $('#selArrMth').trigger('change');
 document.getElementById('selArrDay').selectedIndex = cd;
 $('#selArrDay').trigger('change');
-$('#selNumNights').val('3')
+$('#selNumNights').val(cs)
 $('#selNumNights').trigger('change');
-$('#selArrDay').css('background-color', 'yellow')
-$('#MainContentPlaceHolder_LocationList').val('Pacific Rim')
-$('#MainContentPlaceHolder_MapList').val('94')
+//$('#selArrDay').css('background-color', 'yellow')
+$('#MainContentPlaceHolder_LocationList').val('Pacific Rim')//$('#MainContentPlaceHolder_MapList').val('94')
 $('#selEquipmentSub').val('Small Tent')
-$('#selPartySize').val('2')//$('#selResource').val(mysites[0])
+$('#selPartySize').val('2') //$('#selResource').val(mysites[0])
 //$('#selResource').trigger('change');
-mychange()
-
-/*
+mychange()/*
 $(document).ready(function () {
   $('#reserveButton').change(function () {
     alert('Button changed')
