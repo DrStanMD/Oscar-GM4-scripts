500a9f8a95b9b3e5b60f4028abeca54e6162cc0b
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2020-03-21 07:50:24 -0700

    Update Prefs.user.js

--- Prefs.user.js
+++ Prefs.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Prefs
-// @version  15
+// @version  15.1
 // @include *provider/providerpreference.jsp*
 // @require   http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js
 // @grant    none
@@ -9,11 +9,26 @@
 //========Get Path============
 var elements = (window.location.pathname.split('/', 2))
 firstElement = (elements.slice(1) )
-//alert(firstElement)
 vPath = ("https://" + location.host + "/"  + firstElement + "/")
-//alert(vPath)
 //*****************************************************************
+//=====Get Parameters============
+var params = {};
+if (location.search) {
+    var parts = location.search.substring(1).split('&');
+    for (var i = 0; i < parts.length; i++) {
+        var nv = parts[i].split('=');
+        if (!nv[0]) continue;
+        params[nv[0]] = nv[1] || true;
+    }
+}
+//alert(params.time)
+//****************************
 
 $('input[name=\'every_min\']').css('background-color', 'yellow');
 $('input[name=\'every_min\']').focus()
 //alert($('input[name=\'every_min\']').val())
+$('input[name=\'every_min\']').val(params.time)
+
+setTimeout(function(){ 
+$('body > form:nth-child(1) > div:nth-child(3) > input:nth-child(1)').click()
+}, 500);
