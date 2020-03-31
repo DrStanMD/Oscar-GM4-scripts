eebfbc733fe57632e5c2a2f6bb2f0aea65f10177
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2020-03-30 23:02:29 -0700

    Update BillingDefaults.user.js

--- BillingDefaults.user.js
+++ BillingDefaults.user.js
@@ -16,39 +16,35 @@ $("input[name='facilityNum']").val("OADDD"); //your facility number goes here
 //$("input[name='xml_diagnostic_detail3']").attr('disabled','disabled');
 
 
-var theDefault='Refer To'; 
+var theDefault = 'Refer To';
 var theOptions = document.getElementsByName('refertype1')[0].options;
 
-for  (var theOption of theOptions)
-{for (var i=0; i<3; i++) {}
-	if(typeof(theOption)=='object'){
-		if(theOption.text==theDefault){
-			theOption.selected=true;
-			break;
-		}
-	}
+for (var theOption of theOptions) {
+    if (typeof(theOption) == 'object') {
+        if (theOption.text == theDefault) {
+            theOption.selected = true;
+            break;
+        }
+    }
 }
 
 
 //===========Cookies===============
-function setCookie(cname,cvalue,exdays,cpath)
-{
-var d = new Date();
- d.setTime(d.getTime()+(exdays*24*60*60*1000));
-var expires = "expires="+d.toGMTString();
-document.cookie = cname + "=" + cvalue + "; " + expires +  "; " + cpath
- }
-
-function getCookie(cname)
-{
-var name = cname + "=";
-var ca = document.cookie.split(';');
-for(var i=0; i<ca.length; i++) 
-  {
-  var c = ca[i].trim();
-  if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+function setCookie(cname, cvalue, exdays, cpath) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + "; " + expires + "; " + cpath
 }
-return "";
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
 }
 //********************************************************
 
@@ -56,66 +52,66 @@ var default_doc = null
 //default_doc = getCookie("default_doc")
 default_doc = localStorage.getItem("default_doc");
 
-if ($('select[name=xml_provider]').val()=="000000"){
-showAlert7()
+if ($('select[name=xml_provider]').val() == "000000") {
+    showAlert7()
 }
 
-var input8=document.createElement("input");
-input8.type="button";
-input8.value="Store default Provider";
+var input8 = document.createElement("input");
+input8.type = "button";
+input8.value = "Store default Provider";
 input8.onclick = showAlert8;
 input8.setAttribute("style", "font-size:12px;position:absolute;top:15px;right:0px;");
-document.body.appendChild(input8); 
-function showAlert8(){  
-var Set_Default = $('select[name=xml_provider] option:selected').text()
+document.body.appendChild(input8);
 
-qdocname = Set_Default
-//qdocname = window.prompt("Enter Doctor's name", Set_Default);
-//setCookie("default_doc",qdocname,360,"path=/");  //set cookie to expire in 360 days
-localStorage.setItem("default_doc",qdocname); 
-alert(qdocname +" is now set as the default billing doctor on this computer.")
+function showAlert8() {
+    var Set_Default = $('select[name=xml_provider] option:selected').text()
 
-location.reload();
+    qdocname = Set_Default
+    //qdocname = window.prompt("Enter Doctor's name", Set_Default);
+    //setCookie("default_doc",qdocname,360,"path=/");  //set cookie to expire in 360 days
+    localStorage.setItem("default_doc", qdocname);
+    alert(qdocname + " is now set as the default billing doctor on this computer.")
+
+    location.reload();
 }
 
-function showAlert7(){
-//default_doc ='Hurwitz, Stanley D';
-if (!default_doc){
-alert("The default provider is not set for this session on this computer.  \nSelect the default provider from the list, then click STORE.")
-}
-    
- $('select[name=xml_provider]').css("background-color","yellow");
+function showAlert7() {
+    //default_doc ='Hurwitz, Stanley D';
+    if (!default_doc) {
+        alert("The default provider is not set for this session on this computer.  \nSelect the default provider from the list, then click STORE.")
+    }
 
-//var theDefault='Hurwitz, Stanley D';
-var theDefault=default_doc  
-var theOptions = document.getElementsByName('xml_provider')[0].options;
+    $('select[name=xml_provider]').css("background-color", "yellow");
 
-for  (var theOption of theOptions)
-{for (var i=0; i<3; i++) {}
-	if(typeof(theOption)=='object'){
-		if(theOption.text==theDefault){
-			theOption.selected=true;
-			break;
-		}
-	}
-}
+    //var theDefault='Hurwitz, Stanley D';
+    var theDefault = default_doc
+    var theOptions = document.getElementsByName('xml_provider')[0].options;
+
+    for (var theOption of theOptions) {
+        if (typeof(theOption) == 'object') {
+            if (theOption.text == theDefault) {
+                theOption.selected = true;
+                break;
+            }
+        }
+    }
 
 
-var today = new Date();
+    var today = new Date();
     var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
+    var mm = today.getMonth() + 1; //January is 0!
     var yyyy = today.getFullYear();
-    if(dd<10){
-        dd='0'+dd
-    } 
-    if(mm<10){
-        mm='0'+mm
-    } 
-    var today = yyyy+'-'+mm+'-'+dd;
-$("input[name='xml_appointment_date']").val(today);
-$('#xml_appointment_date').css("background-color","yellow");
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+    var today = yyyy + '-' + mm + '-' + dd;
+    $("input[name='xml_appointment_date']").val(today);
+    $('#xml_appointment_date').css("background-color", "yellow");
 }
 
 //Highlight billing codes
 var allergy = "body > form:nth-child(5) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(7) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(1)"
-$(allergy).css("background-color","lightyellow");
+$(allergy).css("background-color", "lightyellow");
