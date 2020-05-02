e0a4ebb8f25f37f9661bed2a0877127feda2cabc
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2020-05-01 18:30:02 -0700

    Create ResizeMHA.user.js

---
+++ ResizeMHA.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Resize MyHealthAccess app 
+// @namespace   Stanscripts
+// @description Resizes MyHealthAccess app to
+// @include   *myhealthaccess.ca/app/#/clinic*
+// @grant       none
+// ==/UserScript==
+
+window.resizeTo(600, 600);
+window.moveTo(0, 0)
