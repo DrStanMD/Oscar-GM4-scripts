97f961c436de9069fa2167cb779463e95698b83a
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2020-05-11 19:50:47 -0700

    Update Not_assigned.user.js

--- Not_assigned.user.js
+++ Not_assigned.user.js
@@ -362,7 +362,7 @@ input11.value = 'Save and Close';
 var ShowClick = (input11.value).trim()
 input11.id = 'Forms'
 input11.onclick = showAlert11;
-input11.setAttribute('style', 'font-size:12px;position:absolute;top:460px;right:180px;height:40px;background-color:  #FF00FF  ');
+input11.setAttribute('style', 'font-size:12px;position:absolute;top:460px;right:0px;height:40px;background-color:  #FF00FF  ');
 document.body.appendChild(input11);
 function showAlert11() {
   //$(TicklerButton).click()
