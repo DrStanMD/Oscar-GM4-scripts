a5b5c0a696edf82799e7ef768661c838d0fbc212
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2020-06-29 07:55:56 -0700

    Update pagescraper-with-videolink.user.js

--- pagescraper-with-videolink.user.js
+++ pagescraper-with-videolink.user.js
@@ -5,7 +5,7 @@
 // @include     */casemgmt/forward.jsp?action=view&demographic*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
 // @grant       none
-// @version 15.6
+// @version 15.7
 // ==/UserScript==
 
 //Reserve line in header
