5fe0f5626dd8e43e7cd53806bee6a9b273cf18a3
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2020-09-13 07:55:18 -0700

    Update INR_Automation.user.js

--- INR_Automation.user.js
+++ INR_Automation.user.js
@@ -6,5 +6,22 @@
 // @version     15.1
 // @grant       none
 // ==/UserScript==
+
+//=====Get Parameters============
+//vPath = '../'
+var params = {};
+if (location.search) {
+    var parts = location.search.substring(1).split('&');
+    for (var i = 0; i < parts.length; i++) {
+        var nv = parts[i].split('=');
+        if (!nv[0]) continue;
+        params[nv[0]] = nv[1] || true;
+    }
+}
+
 var y = ($('.note > td:nth-child(3)').html())
 $('input[name=\'value(inputValue-0)\']').val(y)
+
+str = params.instructions.replace(/%20/g, ' ');
+//alert(str)
+$("input[name='value(comments-0)']").val(str);
