c846de39f03bf4c71ee8e7bc473f61a854e63657
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2021-08-28 08:35:27 -0700

    Update reminders_.user.js

--- reminders_.user.js
+++ reminders_.user.js
@@ -17,8 +17,14 @@
 /*
 open about:config and set the following to true.
 dom.allow_scripts_to_close_windows
+
 Requires Push to Reminders eform found at:
 https://github.com/DrStanMD/Oscar-GM4-scripts/blob/master/Push_to_Reminder.zip
+
+Edit the "Push to Reminder" eform and mark the following boxes as checked:
+Show Only Latest Revision of eForm Template
+Show Only Latest Revision of eForm Instance
+Patient Independent
 */
 
 var inputvar = 226 //"Push to Reminders" form id goes here
