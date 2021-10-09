c32065e83769a5c8dcff98639f318955c271b6f1
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2021-10-09 04:25:59 -0700

    Add files via upload

---
+++ calendar.js
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>{name} - {'greasemonkey_user_script_editor'|i18n}</title>
+<link rel="stylesheet" href="/third-party/shoelace/shoelace.css">
+<link rel="stylesheet" href="/third-party/font_awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="./edit-user-script.css">
+
+<script src="/src/i18n.js"></script>
+<script src="/third-party/tinybind/tinybind.min.js"></script>
+<script src="/src/util/get-best-locale-match.js"></script>
+<script src="/src/util/i18n-user-script.js"></script>
+<script src="/src/util/rivets-formatters.js"></script>
+
+<script src="/third-party/codemirror/codemirror.js"></script>
+<link rel="stylesheet" href="/third-party/codemirror/codemirror.css">
+
+<script src="/third-party/codemirror/mode/javascript/javascript.js"></script>
+
+<script src="/third-party/codemirror/addon/dialog/dialog.js"></script>
+<link rel="stylesheet" href="/third-party/codemirror/addon/dialog/dialog.css">
+
+<script src="/third-party/codemirror/addon/comment/comment.js"></script>
+
+<script src="/third-party/codemirror/addon/scroll/annotatescrollbar.js"></script>
+
+<script src="/third-party/codemirror/addon/search/searchcursor.js"></script>
+<script src="/third-party/codemirror/addon/search/search.js"></script>
+<script src="/third-party/codemirror/addon/search/match-highlighter.js"></script>
+<script src="/third-party/codemirror/addon/search/jump-to-line.js"></script>
+
+<script src="/third-party/codemirror/addon/lint/lint.js"></script>
+<link rel="stylesheet" href="/third-party/codemirror/addon/lint/lint.css">
+<script src="./cm-addons/lint-metadata.js"></script>
+<script src="./cm-addons/lint-metadata.run.js"></script>
+
+<script src="/src/downloader.js"></script>
+<script src="/src/parse-meta-line.js"></script>
+<script src="/src/parse-user-script.js"></script>
+<script src="/src/supported-apis.js"></script>
+<script src="/third-party/convert2RegExp.js"></script>
+<script src="/third-party/MatchPattern.js"></script>
+</head>
+
+<body>
+
+<div id="modal-splash">
+  <div id="modal">
+    <header>
+      <span class="fa fa-cog" rv-class-fa-spin="modal.closeDisabled"></span>
+      {modal.title}
+    </header>
+
+    <div rv-show="modal.errorList | empty">
+      <progress></progress>
+    </div>
+    <div rv-show="modal.errorList | empty | not">
+      <p>{'script_save_error'|i18n}</p>
+      <ul>
+        <li rv-each-error="modal.errorList">{error}</li>
+      </ul>
+      <p>{'fix_and_save'|i18n}</p>
+    </div>
+
+    <footer>
+      <button
+          class="button button-info button-big"
+          id="modal-close"
+          rv-disabled="modal.closeDisabled">
+        {'close'|i18n}
+      </button>
+    </footer>
+  </div>
+</div>
+
+<div class="tab-bar">
+  <div id="save" class="command-item" rv-title="'save'|i18n" role="button">
+    <i class="icon fa fa-floppy-o" aria-hidden="true"></i>
+  </div>
+  <ul id="tabs"></ul>
+</div>
+<div id="editor"></div>
+
+<script src="edit-user-script.js"></script>
+
+</body>
+</html>
