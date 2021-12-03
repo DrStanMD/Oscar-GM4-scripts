37ad80a073d3762ef5eaa0742e8ce8f50ab9ca5c
DrStanMD <34532846+DrStanMD@users.noreply.github.com>
2021-12-03 12:40:40 -0800

    Update TicklerDefaults.user.js

--- TicklerDefaults.user.js
+++ TicklerDefaults.user.js
@@ -144,7 +144,7 @@ function showAlert1() {
     }
 
     x = document.getElementsByName('textarea')
-    $(x[0]).val('Book CDM telehealth 30 min in 2021')
+    $(x[0]).val('Book CDM telehealth 30 min in 2022')
 
     var theDefault = 'Hurwitz, Office';
     var theOptions = document.getElementsByName('task_assigned_to')[0].options;
